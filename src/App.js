f047b813510793d4e2595388e0c05b83567fd214
Matias Andrada <matiasandrada5622@gmail.com>
2022-03-09 18:59:47 -0300

    Error en el Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import Header from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
