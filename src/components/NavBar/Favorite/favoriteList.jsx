9116799c5964adba65faaa67d7eb50b66a61676f
matiasAndrada <matiasandrada5622@gmail.com>
2023-08-10 22:13:21 -0300

    2.0.0

--- src/components/NavBar/Favorite/favoriteList.jsx
+++ src/components/NavBar/Favorite/favoriteList.jsx
@@ -6,7 +6,6 @@ import { FaTrash } from 'react-icons/fa';
 export const FavoriteList = ({ idUser }) => {
   const dispatch = useDispatch();
   const { listMoviesFav, error, loading } = useSelector((state) => state.favoriteMovies);
-  console.log(listMoviesFav)
 
   useEffect(() => {
     dispatch(getFavoriteMovies(idUser));
@@ -15,17 +14,19 @@ export const FavoriteList = ({ idUser }) => {
   function deleteMovie() {
     dispatch(deleteFavoriteMovies(idUser));
   }
-  /*
+  
     if (error) {
       return (
         <div className="alert alert-danger text-center mt-4" role="alert">
           {error}
         </div>
       );
-    }*/
+    }
+    
 
   return (
     <div className="moviesFavList">
+      
       {loading ? (
         <div className="flex items-center justify-center">
           <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-blue-500"></div>
@@ -51,7 +52,6 @@ export const FavoriteList = ({ idUser }) => {
               </button>
 
             </>
-
           ) : (
             <div className="bg-red-200 text-red-800 text-center mt-4 py-2 rounded">
               No movies in favorites
