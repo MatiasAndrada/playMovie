06ba3f7535ac91b4c52662b9ccea30cc94388faa
matiasAndrada <matiasandrada5622@gmail.com>
2023-08-15 12:30:29 -0300

    folder build copy the branch master

--- src/components/MyAccount/MyAccount.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyAccount = () => {
-  return (
-    <div>Esta pagina es de account</div>
-  )
-}
-
-export default MyAccount
--- src/components/router/privateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-//redux
-import { useSelector } from "react-redux";
-
-export function PrivateRoute({ children }) {
-  const auth = useSelector((state) => state.auth.activo);
-  return auth ? children : <Navigate to="/login" />;
-}
